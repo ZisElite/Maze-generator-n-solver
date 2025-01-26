b45e7ce9ee85eace94c066e989054d1843faedad
ZisElite <zisispsalidas.3d@gmail.com>
2025-01-26 23:11:52 +0200

    Added cells

--- data_structures.py
+++ data_structures.py
@@ -5,6 +5,9 @@ class Point:
     def __init__(self, x:int, y:int):
         self.x = x
         self.y = y
+    
+    def __repr__(self):
+        return f"Point: x={self.x} y={self.y}"
 
 class Line:
     def __init__(self, start:Point, end:Point):
@@ -12,7 +15,11 @@ class Line:
         self.end = end
     
     def draw(self, canvas:Canvas, fill_color:str):
+        print(self)
         canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2)
+    
+    def __repr__(self):
+        return f"Line: start[{self.start}] end[{self.end}]"
 
 class Window:
     def __init__(self, width, height):
@@ -20,7 +27,8 @@ class Window:
         self.app.title = "Mazed"
         self.app.minsize(width, height)
         self.app.maxsize(width, height)
-        self.canvas = Canvas(self.app)
+        self.canvas = Canvas(self.app, width=width, height=height)
+        self.canvas
         self.canvas.pack()
         self.running = False
         self.app.protocol("WM_DELETE_WINDOW", self.close)
@@ -39,3 +47,43 @@ class Window:
     
     def draw_line(self, line:Line, fill_color:str):
         line.draw(self.canvas, fill_color)
+
+class Cell:
+    def __init__(self, x1, y1, x2, y2, win:Window):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self._win = win
+        self. has_left_wall = True
+        self. has_right_wall = True
+        self. has_top_wall = True
+        self. has_bottom_wall = True
+    
+    def draw(self):
+        top_left = Point(self._x1, self._y1)
+        top_right = Point(self._x2, self._y1)
+        bot_left = Point(self._x1, self._y2)
+        bot_right = Point(self._x2, self._y2)
+        top_line = Line(top_left, top_right)
+        bot_line = Line(bot_left, bot_right)
+        left_line = Line(top_left, bot_left)
+        right_line = Line(top_right, bot_right)
+        if self.has_top_wall:
+            print("top")
+            self._win.draw_line(top_line, "black")
+        if self.has_bottom_wall:
+            print("bot")
+            self._win.draw_line(bot_line, "black")
+        if self.has_left_wall:
+            print("left")
+            self._win.draw_line(left_line, "black")
+        if self.has_right_wall:
+            print("right")
+            self._win.draw_line(right_line, "black")
+        
+    def draw_move(self, to_cell:Self, undo=False):
+        center1 = Point((self._x1 + self._x2)//2, (self._y1 + self._y2)//2)
+        center2 = Point((to_cell._x1 + to_cell._x2)//2, (to_cell._y1 + to_cell._y2)//2)
+        color = "gray" if undo else "red"
+        self._win.draw_line(Line(center1, center2), color)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from data_structures import Line, Point, Window
+from data_structures import Line, Point, Window, Cell
 def main():
     win = Window(800, 600)
-    win.draw_line(Line(Point(40, 50), Point(460, 380)), "black")
+    cell1 = Cell(100, 100, 300, 300, win)
+    cell2 = Cell(300, 100, 500, 300, win)
+    cell1.draw()
+    cell2.draw()
+    cell1.draw_move(cell2)
     win.wait_for_close()
 
 if __name__ == "__main__":
