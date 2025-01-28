82b3352356b071b6ebbdf00d89fc1afb2112ca62
Zisis Psalidas <zisispsalidas.3d@gmail.com>
2025-01-28 12:29:01 +0200

    Enabled random generation

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from data_structures import Window, Maze
+import random as rd
 def main():
     win = Window(800, 600)
-    maze = Maze(0, 0, 24, 32, 25, 25, win)
-    #maze = Maze(0, 0, 12, 16, 50, 50, win)
-    #maze = Maze(0, 0, 6, 8, 100, 100, win)
+    #maze = Maze(0, 0, 24, 32, 25, 25, win, rd.randrange(0, 10000))
+    #maze = Maze(0, 0, 12, 16, 50, 50, win, rd.randrange(0, 10000))
+    maze = Maze(0, 0, 6, 8, 100, 100, win, rd.randrange(0, 10000))
     maze._break_walls(0, 0)
     maze._reset_visited()
     if maze.solve():
